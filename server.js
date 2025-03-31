1eac05252b7f76a0639fed7535795487e3458c3c
Glitch (glitch-hello-node) <none>
2025-03-31 13:19:34 +0000

    🏨🕹 Checkpoint ./server.js:53485103/391 ./views/layouts/main.hbs:53485103/63

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const { engine } = require("express-handlebars");
 app.use(express.static(path.join(__dirname, "public")))
 
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views',  path.join(__dirname, "views"));
+
+app.set('view engine', '.hbs');
+app.set('views', "views");
+app.engine('.hbs', engine({extname: '.hbs'}));
 
 app.get('/', (req, res) => {
   res.render('home');
