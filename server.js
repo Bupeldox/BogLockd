99df1faf72e2aa0d14ce8a69f9fe2abceb08e66b
Glitch (glitch-hello-node) <none>
2025-03-31 12:54:24 +0000

    🗼🛣 Checkpoint ./server.js:53485103/138

--- server.js
+++ server.js
@@ -4,15 +4,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path")
 
 const { engine } = require("express-handlebars");
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")))
 
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views',  path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
   res.render('home');
